a00f2f8bae597a975a0acc46a0c35f2358275afb
조훈희 <johunhui@johunhuiui-MacBookAir.local>
2021-03-29 13:58:00 +0900

    make it better

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  const port = 5000
  const { User} = require("./models/User");
  const bodyParser = require('body-parser');
-
- 
+ const config = require('./config/key'); 
  //application/x-www-from-urlencoded(<<이렇게 된 데이터를 분석해서 bodyparser가 클라이언트에서 오는 정보를 서버에서 분석해서 가져올 수 있게 해줌.)
  app.use(bodyParser.urlencoded({extended: true}));
  //application/json(json 타입으로 된 것을 분석해서 가져올 수 있게 해줌.)
@@ -13,12 +12,12 @@
 
 
  const mongoose = require('mongoose')
- mongoose.connect('mongodb+srv://cho44669:qwe123@moviemap3.lajxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+ mongoose.connect(config.mongoURI, {
      useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
  }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
- app.get('/', (req, res) => res.send('Helld world!'))
+ app.get('/', (req, res) => res.send('Helld world! 환영합니다.'))
 
  //주소 마지막 end 포인터 /register
  app.post('/register', (req, res) => {
